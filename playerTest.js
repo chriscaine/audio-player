e1e0a42976a2f273492f1bcd08c9a0bedd26a88f
root <root@raspberrypi.(none)>
2016-04-03 20:23:55 +0000

    adds fluent-ffmpeg

--- playerTest.js
+++ playerTest.js
@@ -1,9 +1,23 @@
 var fs = require('fs');
+var ffmpeg = require('fluent-ffmpeg');
 var lame = require('lame');
 var Speaker = require('speaker');
 
+// fs.createWriteStream -- not working
+// also using Speaker in pipe is not working
+// is there a way to alter format in pipe?
+
+var command = ffmpeg('test.mp3')
+	.output('a file.wav')
+	.audioCodec('pcm_s16le')
+	.run();
+
+
+//command.pipe(new Speaker);
+/*
 fs.createReadStream('test.mp3')
 	.pipe(new lame.Decoder)
 	.on('format', console.log)
 	.pipe(new Speaker);
-
+*/
+// pcm_s16le
