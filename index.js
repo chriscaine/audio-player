676f7e11bd79ea91f91f102294585d772f0f6dc1
Chris Caine <chris@ovationincentives.com>
2016-04-29 14:02:46 +0100

    Wires up socket.io events

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 ﻿"use strict";
 var Rx = require('rxjs/Rx');
 
-var fs = require('fs');
-var meta = require('musicmetadata');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -17,15 +15,72 @@ collection.Load();
 app.use(express.static('public'));
 http.listen(8080);
 
+var CTRLS = {
+    PLAY: 'PLAY',
+    PAUSE:'PAUSE',
+    STOP: 'STOP',
+    STOPAFTER: 'STOPAFTER'
+}
 
-io.on('connection', function(socket) {
-	console.log('connection');
+var App = function(io) {
+    var _io = io;
+    this.Play = function(data) {
+        console.log(data);
+        if(data.id) {
+            // play selected
+        } else {
+            // play next
+        }
+    }
+    this.Pause = function(data) {
+        console.log(data);
+    }
+    this.Stop = function(data) {
+        console.log(data);
+    }
+    this.StopAfter = function(data) {
+        console.log(data);
+    }
 
- socket.on('searchquery', function (search) {
-     
-     if (search.length > 2) {
-         socket.emit('searchresult', { result: collection.Query(search) });
-     }
- });
+    this.PlaylistSync = function(playlist) {
 
-});
+        // collection.SyncPlaylist(playlist);
+        //_io.emit('playlist:tracksubset', collection.GetSubset());
+        _io.emit('playlist:tracksubset', playlist);
+    }
+
+
+
+    return this;
+}
+
+var app = new App(io);
+
+    io.on('connection', function(socket) {
+        console.log('connection');
+ 
+        var searchQuery$ = Rx.Observable.fromEvent(socket, 'search:query');
+
+        searchQuery$.subscribe(function (search) {
+            if (search.length > 2) {
+                socket.emit('search:result', { result: collection.Query(search) });
+            }
+        });
+    
+        var transportCtrl$ = Rx.Observable.fromEvent(socket, 'transport');
+        var play$ = transportCtrl$.filter(e => e.type === CTRLS.PLAY).map(e => e.data);
+        var pause$ = transportCtrl$.filter(e => e.type === CTRLS.PAUSE).map(e => e.data);
+        var stop$ = transportCtrl$.filter(e => e.type === CTRLS.STOP).map(e => e.data);
+        var stopAfter$ = transportCtrl$.filter(e => e.type === CTRLS.STOPAFTER).map(e => e.data);
+      
+        var playlistSync$ = Rx.Observable.fromEvent(socket, 'playlist:sync');
+        
+        play$.subscribe(app.Play);
+        pause$.subscribe(app.Pause);
+        stop$.subscribe(app.Stop);
+        stopAfter$.subscribe(app.StopAfter);
+
+        playlistSync$.subscribe(app.PlaylistSync);
+
+
+    });
